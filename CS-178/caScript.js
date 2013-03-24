2d9a3ced384d1f87135290fd2ee3f2ae5878a76d
embarnard <emiliebarnard@gmail.com>
2013-03-23 18:05:23 -0700

    Trying JQuery again...

--- CS-178/caScript.js
+++ CS-178/caScript.js
@@ -1,9 +1,6 @@
 <script type="text/javascript" src="jquery-1.2.6.min.js"></script>
 
-<script type="text/javascript">
- 
 $(document).ready(function(){
  $("#msgid").html("This is Hello World by JQuery");
 });
  
-</script>
