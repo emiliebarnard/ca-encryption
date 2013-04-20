c8ad980932876a2a52ae7cd8522dc7a072686601
embarnard <emiliebarnard@gmail.com>
2013-04-19 20:17:00 -0700

    Initial Commit
    Project should be fully functional.

--- CS-178/caScript.js
+++
@@ -1,4 +0,0 @@
-$(function() {
-    $( "#selectable" ).selectable();
-  });
-  </script>
---
+++ ca.py
@@ -0,0 +1,76 @@
+#!/usr/local/bin/python
+
+#(c) Emilie Menard Barnard, UC Regents
+#Project for Professor Koc's CS178 at UC Santa Barbara, Winter 2013
+
+#CELLULAR AUTOMATA ENCRYPTION
+print "Content-type: text/html\n"
+
+import copy
+import cgi
+
+#The general rule lookup function helpers:
+
+#This is our 8-bit lookup table:
+lookupTable=([0,0,0,0,0,0,0,0])
+
+#This function creates the lookup table based on the inputted rule number:
+def createLookupTable(ruleNumber):
+	ruleNumber=(bin(ruleNumber))
+	
+	ruleNumberIndex=len(ruleNumber)-1
+	tableIndex=len(lookupTable)-1
+	
+	# Some parsing
+	while ruleNumber[ruleNumberIndex]!='b':
+		lookupTable[tableIndex]=int(ruleNumber[ruleNumberIndex])
+		ruleNumberIndex=ruleNumberIndex-1
+		tableIndex=tableIndex-1
+
+############################################
+
+#My CA function that performs the encryption:
+def ca(caVector, ruleNumber, numSteps):
+
+    for vectorNum in range (0,numSteps+1): #We will produce numSteps many new vectors
+        
+        oldVector=copy.deepcopy(caVector); #Store the old vector values
+        
+        #This will iterate through the elements in the vector and update the values
+        for vectorIndex in range(len(caVector)): 
+            
+            p=(vectorIndex-1)%(len(inputVector)) #wrap around for boundary cases
+            q=vectorIndex%(len(inputVector))
+            r=(vectorIndex+1)%(len(inputVector))
+            
+			# This will generate the new cell using the lookup table:
+            decRep=oldVector[r]+2*oldVector[q]+4*oldVector[p]
+            caVector[vectorIndex]=lookupTable[(len(lookupTable)-1)-decRep]
+            
+            print (repr(caVector[vectorIndex])),
+            
+        if (vectorNum<numSteps):    
+        	print
+
+    return
+
+############################################
+
+#Call the code:
+
+# JS interaction:
+try:
+	form=cgi.FieldStorage()
+	
+	rule=(int(form.getfirst("rule")))%256
+	numSteps=int(form.getfirst("numSteps"))
+
+	inputVector=form.getfirst("inputVector").split(" ")	
+	inputVector=[ int(x) for x in inputVector]
+	
+	createLookupTable(rule)
+	ca(inputVector, rule, numSteps)
+
+# For error debugging:
+except Exception as fail:
+	print "Failure: " + repr(fail)
